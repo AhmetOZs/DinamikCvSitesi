9fa0477ac93e4626ead6c0131bb52cf0177cf5d3
AhmetOZs <114857196+AhmetOZs@users.noreply.github.com>
2022-10-02 13:50:21 +0300

    AdminPanelliCvSitesi(Asp.net(MVC))

---
+++ MvcCv/MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Eğitimlerim()
+        {
+            var egitimlerim = db.TblEğitimlerim.ToList();
+            return PartialView(egitimlerim);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yeteneklerim = db.TblYeteneklerim.ToList();
+            return PartialView(yeteneklerim);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobilerim = db.TblHobilerim.ToList();
+            return PartialView(hobilerim);
+        }
+        public PartialViewResult Sertifikalarım()
+        {
+            var sertifikalarım = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalarım);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(Tbliletisim t)
+        {
+            t.Tarih = DateTime.Parse( DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
